704b03c09658ddec39462b23f136d38d7797e0ee
EliSauder <24995216+EliSauder@users.noreply.github.com>
2022-04-18 13:41:16 -0700

    Working Catch2 Test

---
+++ test/mmath/mmath_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Elijah Sauder on 4/18/22.
+//
+//#include "catch/catch_test_macros.hpp"
+
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("Test Test Case") {
+    REQUIRE(1 == 1);
+}
