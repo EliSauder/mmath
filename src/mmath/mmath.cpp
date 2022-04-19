5592f1d92d49ab1c5deef6b04ef4447d46baced1
EliSauder <24995216+EliSauder@users.noreply.github.com>
2022-04-18 18:50:14 -0700

    Project Setup Working

---
+++ src/mmath/mmath.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Elijah Sauder on 4/18/22.
+//
+#include "mmath/mmath.hpp"
--- test/mmath/mmath_test.cpp
+++ test/mmath/mmath_test.cpp
@@ -6,5 +6,6 @@
 #include <catch2/catch_all.hpp>
 
 TEST_CASE("Test Test Case") {
+
     REQUIRE(1 == 1);
 }
