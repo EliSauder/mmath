bc2129994c91d477d79a757ab1c71a29da437f7d
EliSauder <24995216+EliSauder@users.noreply.github.com>
2022-04-21 16:16:41 -0700

    Updated Build Information Again

--- build/mmath_tests_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/esauder/CLionProjects/mmath/build/mmath_tests_tests-b12d07c.cmake")
-  include("/Users/esauder/CLionProjects/mmath/build/mmath_tests_tests-b12d07c.cmake")
-else()
-  add_test(mmath_tests_NOT_BUILT-b12d07c mmath_tests_NOT_BUILT-b12d07c)
-endif()
